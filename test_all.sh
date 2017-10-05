818e4c55531527c7ab0c299f8f7c33d7f9577871
skavuru <skavuru@ncsu.edu>
2017-10-05 02:10:34 -0400

    Test all works with no device open fails

--- test_all.sh
+++ test_all.sh
@@ -1,28 +1,14 @@
-bash ./test_single.sh 256 8192 3 1 >> test1.txt
-bash ./test_single.sh 256 8192 4 1 >> test2.txt
-bash ./test_single.sh 256 8192 5 1 >> test3.txt
-bash ./test_single.sh 1024 8192 3 1 >> test4.txt
-bash ./test_single.sh 1024 8192 4 1 >> test5.txt
-bash ./test_single.sh 1024 8192 5 1 >> test6.txt
-bash ./test_single.sh 16384 8192 3 1 >> test7.txt
-bash ./test_single.sh 16384 8192 4 1 >> test8.txt
-bash ./test_single.sh 16384 8192 5 1 >> test9.txt
-bash ./test_single.sh 256 8192 3 4 >> test10.txt
-bash ./test_single.sh 256 8192 4 4 >> test11.txt
-bash ./test_single.sh 256 8192 5 4 >> test12.txt
-bash ./test_single.sh 1024 8192 3 4 >> test13.txt 
-bash ./test_single.sh 1024 8192 4 4 >> test14.txt
-bash ./test_single.sh 1024 8192 5 4 >> test15.txt
-bash ./test_single.sh 16384 8192 3 4 >> test16.txt
-bash ./test_single.sh 16384 8192 4 4 >> test17.txt
-bash ./test_single.sh 16384 8192 5 4 >> test18.txt
-bash ./test_single.sh 256 8192 3 16 >> test19.txt
-bash ./test_single.sh 256 8192 4 16 >> test20.txt
-bash ./test_single.sh 256 8192 5 16 >> test21.txt
-bash ./test_single.sh 1024 8192 3 16 >> test22.txt
-bash ./test_single.sh 1024 8192 4 16 >> test23.txt
-bash ./test_single.sh 1024 8192 5 16 >> test24.txt
-bash ./test_single.sh 16384 8192 3 16 >> test25.txt
-bash ./test_single.sh 16384 8192 4 16 >> test26.txt
-bash ./test_single.sh 16384 8192 5 16 >> test27.txt
+for num_process in {1,4,16}
+do
+for num_objects in {256,1024,16384}
+do 
+for feature in {3,4,5}
+do 
+ sleep 10 
+ bash ./test_single.sh ${num_objects} 8192 ${feature} ${num_process}
+ sleep 10
+done
+done
+done
+
 
